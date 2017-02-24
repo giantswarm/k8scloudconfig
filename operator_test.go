b520b360e343bc56922dbeca1e8a430b7ab4cecb
Michal Rostecki <michal@kinvolk.io>
2017-02-24 12:45:37 +0100

    Add a library for templating cloudconfigs

---
+++ operator_test.go
@@ -0,0 +1,34 @@
+package cloudconfig
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type FakeParams struct {
+	Foo string
+}
+
+func TestRenderAssetContent(t *testing.T) {
+	tests := []struct {
+		assetPath       string
+		params          FakeParams
+		expectedContent []string
+	}{
+		{
+			assetPath:       "templates/test/test_template.yml",
+			params:          FakeParams{Foo: "bar"},
+			expectedContent: []string{"foo: bar"},
+		},
+	}
+
+	for _, tc := range tests {
+		content, err := RenderAssetContent(tc.assetPath, tc.params)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, tc.expectedContent, content, "content should be equal")
+	}
+}
