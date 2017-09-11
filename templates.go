4a5f1f4f9e87389a3152aba8f9c893929394c112
Vaclav Rozsypalek <vaclav@giantswarm.io>
2017-09-11 14:19:10 +0200

    coreos hardening step1

--- templates.go
+++ templates.go
@@ -914,6 +914,10 @@ coreos:
     content: |
       {{range .Content}}{{.}}
       {{end}}{{end}}
+  - name: sshd.service
+    enable: true
+  - name: sshd.socket
+    mask: true
   - name: wait-for-domains.service
     enable: true
     command: start
@@ -1134,9 +1138,15 @@ coreos:
     command: stop
     mask: true
   - name: locksmithd.service
+    enable: false
     command: stop
     mask: true
   - name: fleet.service
+    enable: false
+    mask: true
+    command: stop
+  - name: fleet.socket
+    enable: false
     mask: true
     command: stop
   - name: systemd-networkd-wait-online.service
