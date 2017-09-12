e7b9199cebadbc526625e9aa1b2241af3ff1248e
Vaclav Rozsypalek <calvin_24@seznam.cz>
2017-09-12 13:41:44 +0200

    coreos hardening step1 (#185)
    * coreos hardening step1
    
    * coreos-hardening

--- templates.go
+++ templates.go
@@ -926,6 +926,12 @@ coreos:
     content: |
       {{range .Content}}{{.}}
       {{end}}{{end}}
+  - name: sshd.service
+    enable: true
+  - name: sshd.socket
+    enable: false
+    mask: true
+    command: stop
   - name: wait-for-domains.service
     enable: true
     command: start
@@ -1146,9 +1152,15 @@ coreos:
     command: stop
     mask: true
   - name: locksmithd.service
+    enable: false
     command: stop
     mask: true
   - name: fleet.service
+    enable: false
+    mask: true
+    command: stop
+  - name: fleet.socket
+    enable: false
     mask: true
     command: stop
   - name: systemd-networkd-wait-online.service
