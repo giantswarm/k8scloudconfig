9c793fe90c67f5633c62584c87cd63a74b5d991a
JG² <garciagonzalez.julien@gmail.com>
2017-09-29 16:20:41 +0200

    k8s CIS benchmark: audit (#209)
    * 1.1.16: audit-log-path
    
    * 1.1.17: audit-log-maxage
    
    * 1.1.18: audit-log-maxbackup
    
    * 1.1.19: audit-log-maxsize
    
    * fix indent

--- v_0_1_0/master_template.go
+++ v_0_1_0/master_template.go
@@ -1281,7 +1281,11 @@ coreos:
       --tls-cert-file=/etc/kubernetes/ssl/apiserver-crt.pem \
       --tls-private-key-file=/etc/kubernetes/ssl/apiserver-key.pem \
       --client-ca-file=/etc/kubernetes/ssl/apiserver-ca.pem \
-      --service-account-key-file=/etc/kubernetes/ssl/service-account-key.pem
+      --service-account-key-file=/etc/kubernetes/ssl/service-account-key.pem \
+      --audit-log-path=/var/log/apiserver/audit.log \
+      --audit-log-maxage=30 \
+      --audit-log-maxbackup=10 \
+      --audit-log-maxsize=100
       ExecStop=-/usr/bin/docker stop -t 10 $NAME
       ExecStopPost=-/usr/bin/docker rm -f $NAME
   - name: k8s-controller-manager.service
