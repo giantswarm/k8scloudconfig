904af685bbca6dfe10a976d8b334016bea3299c6
Federico Gimenez <fgimenez@users.noreply.github.com>
2017-10-26 15:19:13 +0200

    remove proxy protocol config (#226)

--- v_0_1_0/master_template.go
+++ v_0_1_0/master_template.go
@@ -726,7 +726,6 @@ write_files:
     data:
       server-name-hash-bucket-size: "1024"
       server-name-hash-max-size: "1024"
-      use-proxy-protocol: "true"
 - path: /srv/ingress-controller-dep.yml
   owner: root
   permissions: 0644
