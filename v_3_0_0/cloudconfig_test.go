fdef67ce08039d9fcadcb93845b26c3333e4ef1a
Ross Fairbanks <ross@giantswarm.io>
2017-12-21 11:51:32 +0000

    Add tests

--- v_3_0_0/cloudconfig_test.go
+++ v_3_0_0/cloudconfig_test.go
@@ -19,12 +19,22 @@ func TestCloudConfig(t *testing.T) {
 			params: Params{
 				Extension: &FakeExtension{},
 			},
+			expectedEtcdPort: 443,
 		},
 		{
 			template: WorkerTemplate,
 			params: Params{
 				Extension: &FakeExtension{},
 			},
+			expectedEtcdPort: 443,
+		},
+		{
+			template: WorkerTemplate,
+			params: Params{
+				EtcdPort:  2379,
+				Extension: &FakeExtension{},
+			},
+			expectedEtcdPort: 2379,
 		},
 	}
 
@@ -37,6 +47,10 @@ func TestCloudConfig(t *testing.T) {
 		cloudConfig, err := NewCloudConfig(c)
 		if err != nil {
 			t.Fatal(err)
+		}
+
+		if cloudConfig.params.EtcdPort != tc.expectedEtcdPort {
+			t.Errorf("expected etcd port %q, got %q", tc.expectedEtcdPort, cloudConfig.params.EtcdPort)
 		}
 
 		if err := cloudConfig.ExecuteTemplate(); err != nil {
