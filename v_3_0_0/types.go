24f8693ebb19e47f0d4c91659412490bce578e3c
Paweł Kopiczko <kopiczko@gmail.com>
2017-12-21 12:07:59 +0100

    v_3_0_0: add fine hyperkube types
    This makes initialization outline nicer.

--- v_3_0_0/types.go
+++ v_3_0_0/types.go
@@ -42,8 +42,6 @@ type Hyperkube struct {
 }
 
 type HyperkubeApiserver struct {
-	HyperkubeDocker
-
 	// BindAddress is a value of the --bind-address flag passed to the
 	// hyperkube apiserver. When BindAddress is empty value of
 	// `${DEFAULT_IPV4}` will be passed.
@@ -53,6 +51,15 @@ type HyperkubeApiserver struct {
 	//
 	// azure-operator sets that to 0.0.0.0. Other operators leave it empty.
 	BindAddress string
+	Docker      HyperkubeDocker
+}
+
+type HyperkubeControllerManager struct {
+	Docker HyperkubeDocker
+}
+
+type HyperkubeKubelet struct {
+	Docker HyperkubeDocker
 }
 
 type HyperkubeDocker struct {
