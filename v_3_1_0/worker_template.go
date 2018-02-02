df984906009fd24eb41ea3d7cdfba423f232bf5a
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-02-02 18:10:48 +0200

    Preload ipvs

--- v_3_1_0/worker_template.go
+++ v_3_1_0/worker_template.go
@@ -128,6 +128,16 @@ write_files:
     {{range .Content}}{{.}}
     {{end}}{{end}}
 
+- path: /etc/modules-load.d/ip_vs.conf
+  owner: root
+  permissions: 644
+  content: |
+    ip_vs
+    ip_vs_rr
+    ip_vs_wrr
+    ip_vs_sh
+    nf_conntrack_ipv4
+
 coreos:
   units:
   {{range .Extension.Units}}
