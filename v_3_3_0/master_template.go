b8989b9034f9eae6fc0beaaca8f7df9dba7d93c0
Roman Sokolkov <roman@giantswarm.io>
2018-05-09 12:24:01 +0200

    v_3_3_0: update hyperkube to 1.10.2

--- v_3_3_0/master_template.go
+++ v_3_3_0/master_template.go
@@ -763,7 +763,7 @@ write_files:
           serviceAccountName: kube-proxy
           containers:
             - name: kube-proxy
-              image: quay.io/giantswarm/hyperkube:v1.10.1
+              image: quay.io/giantswarm/hyperkube:v1.10.2
               command:
               - /hyperkube
               - proxy
@@ -1871,7 +1871,7 @@ write_files:
       priorityClassName: core-pods
       containers:
       - name: k8s-api-server
-        image: quay.io/giantswarm/hyperkube:v1.10.1
+        image: quay.io/giantswarm/hyperkube:v1.10.2
         env:
         - name: HOST_IP
           valueFrom:
@@ -1993,7 +1993,7 @@ write_files:
       priorityClassName: core-pods
       containers:
       - name: k8s-controller-manager
-        image: quay.io/giantswarm/hyperkube:v1.10.1
+        image: quay.io/giantswarm/hyperkube:v1.10.2
         command:
         - /hyperkube
         - controller-manager
@@ -2066,7 +2066,7 @@ write_files:
       priorityClassName: core-pods
       containers:
       - name: k8s-scheduler
-        image: quay.io/giantswarm/hyperkube:v1.10.1
+        image: quay.io/giantswarm/hyperkube:v1.10.2
         command:
         - /hyperkube
         - scheduler
@@ -2354,7 +2354,7 @@ coreos:
       RestartSec=0
       TimeoutStopSec=10
       EnvironmentFile=/etc/network-environment
-      Environment="IMAGE=quay.io/giantswarm/hyperkube:v1.10.1"
+      Environment="IMAGE=quay.io/giantswarm/hyperkube:v1.10.2"
       Environment="NAME=%p.service"
       Environment="NETWORK_CONFIG_CONTAINER="
       ExecStartPre=/usr/bin/docker pull $IMAGE
