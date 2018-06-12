73bfef26c16d01cd4ae7f07a2ed27afb441a9d12
Ross Fairbanks <ross@giantswarm.io>
2018-06-12 12:07:28 +0200

    Fix indents

--- v_3_3_4/master_template.go
+++ v_3_3_4/master_template.go
@@ -911,7 +911,7 @@ write_files:
       kind: ClusterRole
       name: calico-node
       apiGroup: rbac.authorization.k8s.io
-    {{ if not .DisableIngressController -}}
+{{ if not .DisableIngressController -}}
     ---
     ## IC
     kind: ClusterRoleBinding
@@ -940,7 +940,7 @@ write_files:
       kind: Role
       name: nginx-ingress-role
       apiGroup: rbac.authorization.k8s.io
-    {{ end -}}
+{{ end -}}
 - path: /srv/rbac_roles.yaml
   owner: root
   permissions: 0644
@@ -976,7 +976,7 @@ write_files:
           - nodes
         verbs:
           - get
-    {{ if not .DisableIngressController -}}
+{{ if not .DisableIngressController -}}
     ---
     ## IC
     apiVersion: v1
@@ -1080,7 +1080,7 @@ write_files:
           - get
           - create
           - update
-    {{ end -}}
+{{ end -}}
 - path: /srv/psp_policies.yaml
   owner: root
   permissions: 0644
