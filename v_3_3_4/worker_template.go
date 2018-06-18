5e675ba1393af54005f648858b445eec82b8d407
Vaclav Rozsypalek <rozsypalek.vaclav@gmail.com>
2018-06-18 14:43:31 +0200

    fix

--- v_3_3_4/worker_template.go
+++ v_3_3_4/worker_template.go
@@ -248,7 +248,7 @@ coreos:
       [Service]
       TimeoutStartSec=300
       Restart=always
-      RestartSec=0
+      RestartSec=2
       TimeoutStopSec=10
       EnvironmentFile=/etc/network-environment
       Environment="IMAGE=quay.io/giantswarm/hyperkube:v1.10.4"
@@ -316,9 +316,10 @@ coreos:
       --kube-reserved="cpu=200m,memory=250Mi" \
       --system-reserved="cpu=150m,memory=250Mi" \
       --enforce-node-allocatable=pods \
-      --eviction-soft=memory.available<500Mi \
-      --eviction-hard=memory.available<300Mi \      
-      --eviction-max-pod-grace-period=memory.available=90 \
+      --eviction-soft='memory.available<400Mi' \
+      --eviction-hard='memory.available<250Mi' \
+      --eviction-soft-grace-period='memory.available=90s' \
+      --eviction-max-pod-grace-period=90 \
       --v=2"
       ExecStop=-/usr/bin/docker stop -t 10 $NAME
       ExecStopPost=-/usr/bin/docker rm -f $NAME
