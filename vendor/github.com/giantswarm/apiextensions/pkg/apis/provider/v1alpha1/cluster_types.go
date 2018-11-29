5da44ccd4fef1c9e8645d6882ec5b5f418f831fc
Paweł Kopiczko <kopiczko@gmail.com>
2018-11-29 15:23:13 +0000

    vendor: update
    Towards https://github.com/giantswarm/giantswarm/issues/2383.
    
    Follow up of https://github.com/giantswarm/apiextensions/pull/174.

--- vendor/github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1/cluster_types.go
+++ vendor/github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1/cluster_types.go
@@ -12,7 +12,6 @@ type Cluster struct {
 	ID         string            `json:"id" yaml:"id"`
 	Kubernetes ClusterKubernetes `json:"kubernetes" yaml:"kubernetes"`
 	Masters    []ClusterNode     `json:"masters" yaml:"masters"`
-	Vault      ClusterVault      `json:"vault" yaml:"vault"`
 	Version    string            `json:"version" yaml:"version"`
 	Workers    []ClusterNode     `json:"workers" yaml:"workers"`
 }
@@ -48,7 +47,6 @@ type ClusterKubernetes struct {
 	CloudProvider     string                             `json:"cloudProvider" yaml:"cloudProvider"`
 	DNS               ClusterKubernetesDNS               `json:"dns" yaml:"dns"`
 	Domain            string                             `json:"domain" yaml:"domain"`
-	Hyperkube         ClusterKubernetesHyperkube         `json:"hyperkube" yaml:"hyperkube"`
 	IngressController ClusterKubernetesIngressController `json:"ingressController" yaml:"ingressController"`
 	Kubelet           ClusterKubernetesKubelet           `json:"kubelet" yaml:"kubelet"`
 	NetworkSetup      ClusterKubernetesNetworkSetup      `json:"networkSetup" yaml:"networkSetup"`
@@ -56,24 +54,13 @@ type ClusterKubernetes struct {
 }
 
 type ClusterKubernetesAPI struct {
-	AltNames       string `json:"altNames" yaml:"altNames"`
 	ClusterIPRange string `json:"clusterIPRange" yaml:"clusterIPRange"`
 	Domain         string `json:"domain" yaml:"domain"`
-	IP             net.IP `json:"ip" yaml:"ip"`
-	InsecurePort   int    `json:"insecurePort" yaml:"insecurePort"`
 	SecurePort     int    `json:"securePort" yaml:"securePort"`
 }
 
 type ClusterKubernetesDNS struct {
 	IP net.IP `json:"ip" yaml:"ip"`
-}
-
-type ClusterKubernetesHyperkube struct {
-	Docker ClusterKubernetesHyperkubeDocker `json:"docker" yaml:"docker"`
-}
-
-type ClusterKubernetesHyperkubeDocker struct {
-	Image string `json:"image" yaml:"image"`
 }
 
 type ClusterKubernetesIngressController struct {
@@ -114,9 +101,4 @@ type ClusterKubernetesSSHUser struct {
 
 type ClusterNode struct {
 	ID string `json:"id" yaml:"id"`
-}
-
-type ClusterVault struct {
-	Address string `json:"address" yaml:"address"`
-	Token   string `json:"token" yaml:"token"`
 }
