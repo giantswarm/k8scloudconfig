a05aa3487adb65e8d911b93b9ed75a61daff30c6
Ross Fairbanks <ross@giantswarm.io>
2018-12-19 15:02:12 +0000

    Remove CoreDNS manifests

--- v_4_0_0/master_template.go
+++ v_4_0_0/master_template.go
@@ -322,14 +322,6 @@ storage:
         source: "data:text/plain;charset=utf-8;base64,{{  index .Files "k8s-resource/calico-all.yaml" }}"
     {{- end }}
 
-    {{- if not .DisableCoreDNS }}
-    - path: /srv/coredns.yaml
-      filesystem: root
-      mode: 0644
-      contents:
-        source: "data:text/plain;charset=utf-8;base64,{{  index .Files "k8s-resource/coredns.yaml" }}"
-    {{- end }}
-
     {{- if not .DisableIngressController }}
     - path: /srv/default-backend-dep.yaml
       filesystem: root
