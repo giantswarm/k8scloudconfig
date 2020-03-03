b67c25c116de5e211734b8ddc8cb0ba70ef56873
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-03-03 14:59:28 +0200

    First boot always (#642)
    * Make provisioning idempotent
    
    * Update chagelog

--- v_5_2_0/master_template.go
+++ v_5_2_0/master_template.go
@@ -372,6 +372,9 @@ systemd:
 
 storage:
   files:
+    - path: /boot/coreos/first_boot
+      filesystem: root
+
     - path: /etc/ssh/trusted-user-ca-keys.pem
       filesystem: root
       mode: 0644
