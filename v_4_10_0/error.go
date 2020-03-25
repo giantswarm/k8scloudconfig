e4768037ec532aa023e3c132cd7ac132c2ab98d8
jihyuk <jhbok@giantswarm.io>
2020-03-25 15:09:41 +0100

    adding deleted errors

--- v_4_10_0/error.go
+++ v_4_10_0/error.go
@@ -2,6 +2,15 @@ package v_4_10_0
 
 import "github.com/giantswarm/microerror"
 
+var componentNotFoundError = &microerror.Error{
+	Kind: "componentNotFound",
+}
+
+// IsComponentNotFound asserts componentNotFoundError.
+func IsComponentNotFound(err error) bool {
+	return microerror.Cause(err) == componentNotFoundError
+}
+
 var invalidConfigError = &microerror.Error{
 	Kind: "invalidConfigError",
 }
