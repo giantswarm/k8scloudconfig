d84355b5ab537c49e5aaba690e61c96ca3c718c7
Thomas Fussell <thomas@giantswarm.io>
2020-06-01 08:54:25 -0400

    Move runtime cgroups config to kubelet flags (#730)
    * move runtime cgroups config to kubelet flags
    
    * update changelog

--- pkg/template/master_template.go
+++ pkg/template/master_template.go
@@ -313,7 +313,8 @@ systemd:
         --register-with-taints=node-role.kubernetes.io/master=:NoSchedule \
         --kubeconfig=/etc/kubernetes/kubeconfig/kubelet.yaml \
         --node-labels="node.kubernetes.io/master,role=master,ip=${DEFAULT_IPV4},{{.Cluster.Kubernetes.Kubelet.Labels}}" \
-        --v=2
+        --v=2 \
+        --runtime-cgroups=/kubereserved.slice
       [Install]
       WantedBy=multi-user.target
   - name: etcd2.service
