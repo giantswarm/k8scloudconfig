1bdafe247d0707d1ff4efc2c4d3170af1477ab7e
Tuomas Mäkinen <tuomas@giantswarm.io>
2020-11-06 15:52:47 +0200

    match worker_template with master_template

--- pkg/template/worker_template.go
+++ pkg/template/worker_template.go
@@ -228,8 +228,8 @@ systemd:
         while [ "$(kubectl get nodes $(hostname | tr '[:upper:]' '[:lower:]')| wc -l)" -lt "1" ]; do echo "Waiting for healthy k8s" && sleep 20s;done; \
         kubectl label nodes --overwrite $(hostname | tr '[:upper:]' '[:lower:]') node-role.kubernetes.io/worker=""; \
         kubectl label nodes --overwrite $(hostname | tr '[:upper:]' '[:lower:]') kubernetes.io/role=worker; \
-        for l in $(echo {{.Cluster.Kubernetes.Kubelet.Labels}} | tr ',' ' '); do \
-            kubectl label nodes --overwrite $(hostname | tr '[:upper:]' '[:lower:]') $l; \
+        for l in $(echo "{{.Cluster.Kubernetes.Kubelet.Labels}}" | tr "," " "); do \
+            kubectl label nodes --overwrite $(hostname | tr "[:upper:]" "[:lower:]") $l; \
         done'
       [Install]
       WantedBy=multi-user.target
