84f769216ebdbc1c42598eee0185b7546a57cd8c
Thomas Fussell <thomas@giantswarm.io>
2021-04-19 13:41:36 -0400

    Retrieve Calico CRDs externally (#863)
    * test s3 calico crds
    
    * use real bucket
    
    * use github over https instead of s3
    
    * add changelog
    
    * use crds from latest tag

--- pkg/template/master_template.go
+++ pkg/template/master_template.go
@@ -427,7 +427,8 @@ storage:
       filesystem: root
       mode: 0644
       contents:
-        source: "data:text/plain;charset=utf-8;base64,{{  index .Files "k8s-resource/calico-crds.yaml" }}"
+        source: "https://raw.githubusercontent.com/giantswarm/k8scloudconfig/v10.2.0/files/k8s-resource/calico-crds.yaml"
+
     {{- if .CalicoPolicyOnly }}
     - path: /srv/calico-policy-only.yaml
       filesystem: root
