49905f9ef3cb7b5716e4d1c6e61b22b8a3f8dc93
yulianedyalkova <julianedialkova@gmail.com>
2021-10-01 14:39:24 +0300

    Change etcd constraint

--- pkg/key/key.go
+++ pkg/key/key.go
@@ -5,7 +5,7 @@ package key
 const (
 	CalicoPolicyOnlyVersionConstraint             = ">= 3.15.0 < 3.16.0"
 	CalicoVersionConstraint                       = ">= 3.20.0 < 3.21.0"
-	EtcdVersionConstraint                         = ">= 3.4.0 < 3.5.0"
+	EtcdVersionConstraint                         = ">= 3.4.0 < 3.6.0"
 	KubernetesVersionConstraint                   = ">= 1.19.0"
 	KubernetesApiHealthzVersionConstraint         = ">= 0.1.1"
 	KubernetesNetworkSetupDockerVersionConstraint = ">= 0.2.0"
