3788362a6a711573c431bc100a45e9565e7d68ff
yulianedyalkova <julianedialkova@gmail.com>
2021-10-01 13:47:22 +0300

    Bump calico to 3.20.0

--- pkg/key/key.go
+++ pkg/key/key.go
@@ -4,7 +4,7 @@ package key
 // fall within the known-good ranges.
 const (
 	CalicoPolicyOnlyVersionConstraint             = ">= 3.15.0 < 3.16.0"
-	CalicoVersionConstraint                       = ">= 3.19.0 < 3.20.0"
+	CalicoVersionConstraint                       = ">= 3.20.0 < 3.21.0"
 	EtcdVersionConstraint                         = ">= 3.4.0 < 3.5.0"
 	KubernetesVersionConstraint                   = ">= 1.19.0"
 	KubernetesApiHealthzVersionConstraint         = ">= 0.1.1"
