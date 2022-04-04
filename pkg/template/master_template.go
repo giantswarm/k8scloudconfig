34a479a0a1b16796d4fd8734830b4c3c23977797
Christian Bianchi <whites11@gmail.com>
2022-04-04 10:51:34 +0200

    - Added systemd unit to create VPA for kube-proxy.

--- pkg/template/master_template.go
+++ pkg/template/master_template.go
@@ -408,7 +408,6 @@ systemd:
       [Service]
       Type=oneshot
       Restart=on-failure
-      ExecStartPre=/bin/bash -c "while [ ! -f /etc/kubernetes/kubeconfig/addons.yaml ]; do echo 'Waiting for /etc/kubernetes/kubeconfig/addons.yaml to be written' && sleep 10; done"
       ExecStartPre=/bin/bash -c "while ! /opt/bin/kubectl --kubeconfig=/etc/kubernetes/kubeconfig/addons.yaml get crd verticalpodautoscalers.autoscaling.k8s.io; do echo 'Waiting for VPA CRD to exists' && sleep 10; done"
       ExecStart=/opt/bin/kubectl --kubeconfig=/etc/kubernetes/kubeconfig/addons.yaml apply -f /srv/kube-proxy-vpa.yaml
       [Install]
