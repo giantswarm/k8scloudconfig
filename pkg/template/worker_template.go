bd5cab78009c8e93023f441ea647a8cde96b186e
Christian Bianchi <whites11@gmail.com>
2022-08-30 11:43:10 +0200

    Check hostname is up to date on azure before starting the kubelet.

--- pkg/template/worker_template.go
+++ pkg/template/worker_template.go
@@ -212,8 +212,7 @@ systemd:
       Environment="ETCD_KEY_FILE=/etc/kubernetes/ssl/calico/etcd-key"
       EnvironmentFile=/etc/network-environment
       {{- if eq .Cluster.Kubernetes.CloudProvider "azure" }}
-      ExecStartPre=/bin/bash -c 'curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r .compute.osProfile.computerName >/etc/desired-host-name'
-      ExecStartPre=/bin/bash -c 'while HN="$(hostname)" && [ "$HN" != "$(cat /etc/desired-host-name)" ] ;  do sleep 2s ; echo "hostname is unexpected (want $(cat /etc/desired-host-name), got $HN)" ;done;'
+            ExecStartPre=ExecStartPre=/bin/bash -c 'while (curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r .compute.osProfile.computerName >/etc/desired-host-name) && DES="$(cat /etc/desired-host-name)" && [ "$DES" != "" ] && HN="$(hostname)" && [ "$HN" != "$DES" ] ;  do sleep 2s ; echo "hostname is unexpected (want $DES, got $HN)" ;done;'
       {{- end }}
       ExecStart=/opt/bin/kubelet \
         {{ range .Kubernetes.Kubelet.CommandExtraArgs -}}
