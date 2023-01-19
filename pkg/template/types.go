46813456224573c60660df264f6719f4e2460eff
Pau <paurosello@gmail.com>
2023-01-19 12:18:36 +0100

    Add subnet prefix remove pod limit

--- pkg/template/types.go
+++ pkg/template/types.go
@@ -15,6 +15,8 @@ type Params struct {
 	// EnableAWSCNI flag. When set to true will use AWS CNI for pod networking
 	// and Calico only for network policies.
 	EnableAWSCNI bool
+	// AWSCNISubnetPrefixMode set to true when cluster is using Subnet Prefix mode, will remove pod limit per node.
+	AWSCNISubnetPrefixMode bool
 	// EnableCSIMigrationAWS flag. When set to true will use in-tree EBS volumes will be migrated to CSI.
 	EnableCSIMigrationAWS bool
 	// force cgroups v1 on flatcar 3033.2.1 and above
